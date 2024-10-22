4cadd78f489cc64dd68899e34f94172bb4468d02
wesley7886 <124694992+wesley7886@users.noreply.github.com>
2024-10-22 10:50:05 +0200

    batch added and class r_m changed a bit

--- TheatreSystem/Controllers/TheaterShowController.cs
+++ TheatreSystem/Controllers/TheaterShowController.cs
@@ -53,6 +53,46 @@ public class TheaterShowController : ControllerBase
         return CreatedAtAction(nameof(GetShowById), new { id = newShow.TheaterShowID }, newShow);
     }
 
+    [HttpPost]
+    public async Task<IActionResult> CreateShows([FromBody] object input)
+    {
+    if (input == null)
+    {
+        return BadRequest("Input cannot be null.");
+    }
+    var createdShows = new List<TheaterShow>();
+    if (input is List<TheaterShow> listshows)
+    {
+        if (!listshows.Any())
+        {
+            return BadRequest("The list of shows can't be empty.");
+        }
+        foreach (var show in listshows)
+        {
+            await _showService.CreateShow(show);
+            createdShows.Add(show);
+        }
+    }
+    else if (input is Dictionary<string, TheaterShow> ShowsDict)
+    {
+        if (!ShowsDict.Any())
+        {
+            return BadRequest("The dictionary of shows can't be empty.");
+        }
+        foreach (var pair in ShowsDict)
+        {
+            var show = pair.Value;
+            await _showService.CreateShow(show);
+            createdShows.Add(show);
+        }
+    }
+    else
+    {
+        return BadRequest("Input must be either a list of TheaterShow objects or a dictionary.");
+    }
+
+    return CreatedAtAction(nameof(GetShowById), new { ids = createdShows.Select(s => s.TheaterShowID).ToArray() }, createdShows);
+    }
     [HttpPut("{id}")]
     public async Task<IActionResult> UpdateShow(int id, [FromBody] TheaterShow updatedShow)
     {
--- TheatreSystem/Controllers/reservation_manager.cs
+++ TheatreSystem/Controllers/reservation_manager.cs
@@ -3,22 +3,23 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft;
 
-[Authorize(Roles = "Admin")]
-[Route("api/[controller]")]
-[ApiController]
+
+[Route($"api/admin"),]
+
 
 public class Reservation_managegerController:ControllerBase
 {
     private readonly  List<Reservation> _context;
-
-    public Reservation_managegerController(List<Reservation> context)
+    private ISeatService _seatService;
+    public Reservation_managegerController(List<Reservation> context,ISeatService seatService)
     {
         _context = context;
+        _seatService = seatService;
     }
   
 
 
- [HttpGet]
+    [HttpGet]
     public async Task<IActionResult> GetReservations([FromQuery] int? showId, [FromQuery] DateTime? date, [FromQuery] string search)
     {
         var reservationsQuery = _context.AsQueryable();       
@@ -40,16 +41,20 @@ public class Reservation_managegerController:ControllerBase
     }
 
  
-[HttpPut("{id}/mark-as-used")]
-    public async Task<IActionResult> MarkAsUsed(int id)
+    [HttpPut("{id}/mark-as-used")]
+    public async Task<IActionResult> MarkAsUsed(Seat seat)
     {
-        var reservation = _context.Find(x=> x.ReservationID == id);
-        if (reservation == null) return NotFound();
-
-        reservation.used = true;
+        
+        
+        if (seat == null) return NotFound();
+        else
+        {
+            var preneeded = new SeatController(_seatService);
+            var needed = await preneeded.ClaimSeat(seat);
         
 
         return NoContent();
+        }
     }
 
  
